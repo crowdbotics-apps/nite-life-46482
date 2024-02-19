1bb8c33c886e379a27445298953b4eec170ba1a3
Crowdbotics <team@crowdbotics.com>
2024-02-19 23:44:52 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "nite-life-46482",
+  url: "https://nite-life-46482.botics.co",
+  api: "https://nite-life-46482.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
